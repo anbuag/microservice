bba3b19395383bd55ce7298d4b7b7cc6a9e20935
Anbarasan P <132566134+anbuag@users.noreply.github.com>
2023-10-04 11:33:11 +0530

    Delete california directory

--- california/src/main/java/com/spring/california/service/TourNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.spring.california.service;
-
-public class TourNotFoundException extends RuntimeException {
-    public TourNotFoundException(Long id) {
-        super("Tour not found with id: " + id);
-    }
-}
